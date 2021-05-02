91773affdd4c53e29570e2ae159c5333cc6abfae
waiterorg <konjkav79@gmail.com>
2021-05-02 22:42:52 +0430

    initial commit

---
+++ SlideShow/SlideShow/templatetags/slideshow.py
@@ -0,0 +1,12 @@
+from django import template
+from ..models import SlideShow
+
+
+register = template.Library()
+
+
+@register.inclusion_tag("SlideShow/slideshow.html")
+def slideshow():
+    return {
+        'slides': SlideShow.objects.filter(status=True, article__status=True)
+    }
